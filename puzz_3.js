01334015225542adec7c131789c85b791d5d6143
firestorm117 <55616239+firestorm117@users.noreply.github.com>
2020-08-05 11:05:26 +0530

    puzzle 3 fixed centre ring

--- puzz_3.js
+++ puzz_3.js
@@ -1,8 +1,8 @@
 var txts = [
   "Dr.LUCRETIUS HARRINGTON",
   "Dr.Lucretius Harrington was one of the world's leading scientists in field of alien technology.It was in the year 1984 when he found his greatest discovery.",
-  "Dr.Harrington's success was the study of a live alien species 'Nebrai' .His strange results concluded that the alien liked flowers ,sparkling items and strangely feels joy in pressing a fellow scientist light board buttons. ",
-  "Dr.Harrington always kept his destkop locked with strange puzzles.He also liked to play a world famous game which is one of the best sellers till date and also holds a world record for most ported video game title. After all it was released on the day of his greatest discovery.",
+  "Dr.Harrington's to sucess was study of a live alien species 'Nebrai' .His strange results concluded that the alien liked flowers ,sparkling items and strangely feels joy in pressing a fellow scientist light board buttons. ",
+  "Dr.Harrington always kept his destkop locked with strange puzzles.He also liked to play a world famous game which is one of the best sellers till date and also holds a world record for most ported video game tiltle .Afer all it was released on day of his greatest discovery.",
 ];
 const c3=['2','6','R','8','E','9','5','D','K','3'];
 const cc3=[9,2,5];
@@ -55,6 +55,8 @@ $("#message").click(function () {
 });
 $("#turning > area").click(function (e) {
   k = e.target;
+  if (e.target.id==1)
+  return;
   ang = parseInt($(k).attr("s")) + parseInt($(k).attr("angle"));
   $("#c" + k.id).css({ transform: "rotate(" + ang + "deg)" });
   $(k).attr("s", ang);
